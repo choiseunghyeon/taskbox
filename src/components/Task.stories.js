fe5a90e2c61b4bda4a8389b5f33d94e04f326c32
choiseunghyeon <ket8780@gmail.com>
2021-10-27 00:09:19 +0900

    learned storybook document - Toolbars&globals chapter

--- src/components/Task.stories.js
+++ src/components/Task.stories.js
@@ -1,8 +1,30 @@
 import Task from "./Task";
 
+const getGreetingForlocale = locale => {
+  switch (locale) {
+    case "kr":
+      return "안녕하세요";
+    case "fr":
+      return "Bonjour";
+    default:
+      return "Hello";
+  }
+};
+
+export const StoryoWithLocale = (Story, context) => {
+  const greeting = getGreetingForlocale(context.globals.locale);
+  return (
+    <>
+      {greeting}
+      <Story />
+    </>
+  );
+};
+
 export default {
   component: Task,
   title: "Task",
+  decorators: [StoryoWithLocale],
 };
 
 const Template = args => <Task {...args} />;
